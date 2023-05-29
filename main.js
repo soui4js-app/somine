e1d6a3660ebee3e517ab0d89603096d5f57bd5dd
huangjianxiong1 <huangjianxiong1@yy.com>
2023-05-29 13:06:26 +0800

    实现地雷布阵

--- main.js
+++ main.js
@@ -1,17 +1,73 @@
 ﻿import * as soui4 from "soui4";
-
+import * as std from "std";
 var g_workDir="";
 
 class MineBoard{
-	constructor(gridx,gridy,mines){
-		
+	constructor(rows,cols,mines){
+		this.rows = rows;
+		this.cols = cols;
+		this.mines = mines;
+		this.reset();
 	}
+	//state. 0 - unexplored, 1- explored, 2- marked
+	reset(){
+		this.board = [];
+		for(let i=0;i<this.rows;i++){
+			this.board.push([]);
+			for(let j=0;j<this.cols;j++){
+				this.board[i].push({mine:false,state:0});
+			}
+		}
+		for(let i=0;i<this.mines;i++){
+			let x = Math.floor(Math.random()*this.cols);
+			let y = Math.floor(Math.random()*this.rows);
+			this.board[y][x].mine=true;
+		}
+	}
+
+	isMine(x,y){
+		return this.board[y][x].mine;
+	}
+
+	getRoundMines(x,y){
+		let x1=x-1;
+		let x2=x+1;
+		let y1=y-1;
+		let y2=y+1;
+		x1 = Math.max(x1,0);
+		x2 = Math.min(x2,this.cols-1);
+		y1 = Math.max(y1,0);
+		y2 = Math.min(y2,this.rows-1);
+		let ret = 0;
+		for(let i=y1;i<=y2;i++){
+			for(let j=x1;j<=x2;j++){
+				if(this.board[i][j].mine)
+					ret ++;
+			}
+		}
+		return ret;
+	}
+
+
 }
+
+//定义一个全局的信息
+var boardInfo = [
+	[9,9,9],
+	[16,16,20],
+	[30,16,99]
+];
+
+const base_id=1000;
 
 class MainDialog extends soui4.JsHostWnd{
 	constructor(){
 		super("layout:dlg_main");
 		this.onEvt = this.onEvent;
+		this.board = [];
+		for(let i=0;i<3;i++){
+			this.board.push(new MineBoard(boardInfo[i][0],boardInfo[i][1],boardInfo[i][2]));
+		}
 	}
 
 	onEvent(e){
@@ -33,15 +89,34 @@ class MainDialog extends soui4.JsHostWnd{
 		let stackApi = soui4.QiIStackView(stack_board);
 		let id = e.Sender().GetID();
 		stackApi.SelectView(id-200);
+		this.board[id-200].reset();
 		stackApi.Release();
 	}
 
-	buildBoard(size){
+	buildBoard2(size){
 		let head="<t:g.mine>";
 		let tail="</t:g.mine>";
 		let xml="";
 		for(let i=0;i<size;i++){
-			let ele = "<data id=\""+(500+i)+"\" data=\""+(1000+i)+"\"" +" text=\"" + i+"\"" +"/>";
+			let ele = "<data id=\""+(500+i)+"\" data=\""+(base_id+i)+"\"" +" text=\"" + i+"\"" +"/>";
+			xml += head+ele+tail;
+		}
+		return xml;
+	}
+
+	buildBoard(mode){
+		let rows = boardInfo[mode][0];
+		let cols =boardInfo[mode][1];
+		let grids = rows * cols;
+
+		let head="<t:g.mine>";
+		let tail="</t:g.mine>";
+		let xml="";
+		
+		for(let i=0;i<grids;i++){
+			let y = Math.floor(i / cols);
+			let x = i%cols;
+			let ele = "<data id=\""+(500+i)+"\" data=\""+(base_id+i)+"\"" +" text=\"" + this.board[mode].getRoundMines(x,y)+"\"" +"/>";
 			xml += head+ele+tail;
 		}
 		return xml;
@@ -54,15 +129,15 @@ class MainDialog extends soui4.JsHostWnd{
 		soui4.SConnect(this.FindIChildByID(202),soui4.EVT_CMD,this,this.onOptBtn);
 		{
 			let board = this.FindIChildByName("board_easy");
-			board.CreateChildrenFromXml(this.buildBoard(9*9));	
+			board.CreateChildrenFromXml(this.buildBoard(0));	
 		}
 		{
 			let board = this.FindIChildByName("board_middle");
-			board.CreateChildrenFromXml(this.buildBoard(16*16));	
+			board.CreateChildrenFromXml(this.buildBoard(1));	
 		}
 		{
 			let board = this.FindIChildByName("board_hard");
-			board.CreateChildrenFromXml(this.buildBoard(30*16));	
+			board.CreateChildrenFromXml(this.buildBoard(2));	
 		}
 
 		soui4.SConnect(this.FindIChildByName("btn_test"),soui4.EVT_CMD,this,this.onBtnTest);
